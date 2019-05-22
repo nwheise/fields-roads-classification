82d0190c07f0cfc24875e92f5db57920f162de5d
Nick Heise <nick@heise.org>
2019-05-22 19:45:56 +0200

    progress

--- nets.py
+++ nets.py
@@ -5,10 +5,32 @@ import torch.nn.functional as F
 class Net(nn.Module):
 
     def __init__(self):
-        super(MappingNet, self).__init__()
-        self.fc1 = nn.Linear(in_features=2, out_features=2)
+        super(Net, self).__init__()
 
+        # 50x50 input
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5) # 46x46
+        self.maxpool = nn.MaxPool2d(kernel_size=(2, 2)) # 24x24
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5) # 20x20
+        # another maxpool makes it 10x10
+        self.fc1 = nn.Linear(in_channels=12*10*10, out_channels=200)
+        self.fc2 = nn.Linear(in_channels=200, out_channels=50)
+        self.fc3 = nn.Linear(in_channels=50, out_channels=2)
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.maxpool(x)
+
+        x.view(-1, 12*10*10)
+
         x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+
         return x
--- solution.py
+++ solution.py
@@ -1,9 +1,41 @@
+import os
 import torch
 import torchvision
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms, models, utils
+from nets import Net
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+DATA_FOLDER = 'data'
+FIELDS_FOLDER = 'fields'
+ROADS_FOLDER = 'roads'
+TRANSFORMS_FOLDER = 'transforms'
+BATCH_SIZE = 1
+EPOCHS = 5
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
 transform = transforms.Compose([
-    transforms.RandomResizedCrop(224),
+    transforms.Resize(size=100),
+    transforms.RandomResizedCrop(size=75),
     transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms])
+    transforms.ToTensor()
+    ])
+
+classes = ('field', 'road')
+
+train_data = datasets.ImageFolder(DATA_FOLDER, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+for epoch in range(EPOCHS):
+    if not os.path.isdir(os.path.join(DATA_FOLDER, TRANSFORMS_FOLDER)):
+        os.makedirs(os.path.join(DATA_FOLDER, TRANSFORMS_FOLDER))
+
+    for i, data in enumerate(train_loader):
+        to_pil_img = transforms.ToPILImage()
+        img = to_pil_img(data[0][0])
+        img.save(os.path.join(DATA_FOLDER, TRANSFORMS_FOLDER, f'{epoch}_{i}.jpg'))
